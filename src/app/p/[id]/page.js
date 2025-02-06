267e03a3008834203c667dcbcdcc64c0e07ebce0
willswats <william@williamwatson.dev>
2025-02-06 14:10:41 +0000

    Add cache to post

--- src/app/p/[id]/page.js
+++ src/app/p/[id]/page.js
@@ -1,18 +1,28 @@
+import { unstable_cache } from "next/cache";
+
 import prisma from "lib/prisma";
 
 export default async function Page({ params }) {
   // TODO: add error handling for invald routes
+  // TODO invalidate the cache with revalidate on edit
   const { id } = await params;
-  const post = await prisma.post.findUnique({
-    where: {
-      id: id,
-    },
-    include: {
-      author: {
-        select: { name: true },
+  const getPost = () => {
+    return prisma.post.findUnique({
+      where: {
+        id: id,
       },
-    },
+      include: {
+        author: {
+          select: { name: true },
+        },
+      },
+    });
+  };
+  const getCachedPost = unstable_cache(async () => await getPost(), ["post"], {
+    revalidate: 3600,
+    tags: ["post"],
   });
+  const post = await getCachedPost();
 
   return (
     <>
