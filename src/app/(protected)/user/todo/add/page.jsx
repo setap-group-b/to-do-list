4baf443976948c3faed4ca42458e3a9d8aae7db7
Harvey Lopez <up2118183@myport.ac.uk>
2025-03-02 22:20:21 +0000

    Added: Brought branch in line with new major changes to main.

--- src/app/(protected)/user/todo/add/page.jsx
+++ src/app/(protected)/user/todo/add/page.jsx
@@ -1,11 +1,31 @@
+"use client";
 import { TodoForm } from "@/components";
 import { createTodo } from "@/app/actions/todo";
 
+const getDates = () => {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  // getting time units for later use in notification timers etc
+  const hour = date.getHours();
+  const minute = date.getMinutes() % 10;
+  //return (`${year}-${month}-${day}`);
+  return (`${day}-${month}-${year}`);
+}
+  
 export default function UserTodoAdd() {
   return (
     <TodoForm
       formAction={createTodo}
-      initialData={{ title: "", content: "" }}
+      initialData={{
+        title: "",
+        content: "",
+        priority: "",
+        deadline: { getDates },
+      }}
     />
   );
 }
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,19 +1,40 @@
 "use client";
 
 import Link from "next/link";
-import { useActionState } from "react";
+import { useState, useActionState } from "react";
 
 export const TodoForm = ({ formAction, initialData }) => {
   const [formState, action] = useActionState(formAction, {
     errors: {},
   });
 
+  // setup for cycle priority
+  const [priority, setPriority] = useState(todo.priority || "Set Your Priority");
+  const priorities = ["No-Priority", "Low-Priority", "Medium-Priority", "High- Priority"];
+
+  // finds current priority and increments by one
+  const cyclePriority = () => {
+    const currentIndex = priorities.indexOf(priority);
+    const nextIndex = (currentIndex + 1) % priorities.length;
+  }
+  // Assuming that height order of elements is [Title, ]
   return (
-    <>
+    <> 
       <h1>{initialData.title ? "Update" : "Create"} Post</h1>
       <form action={action}>
+
+      <section> {/** upon clicking, the button adheres to cyclePriority, overwriting the default value */}
+          <button 
+            htmlFor="priority" 
+            type="button" 
+            onClick={ cyclePriority }
+            >{ priority }
+          </button>
+        </section>
+
         <section>
           <label htmlFor="title">Title</label>
+          <br></br>
           <input
             type="text"
             id="title"
@@ -25,8 +46,10 @@ export const TodoForm = ({ formAction, initialData }) => {
             <p>{formState.errors.title?.join(", ")}</p>
           )}
         </section>
+
         <section>
           <label htmlFor="content">Content</label>
+          <br></br>
           <textarea
             id="content"
             name="content"
@@ -35,12 +58,27 @@ export const TodoForm = ({ formAction, initialData }) => {
           ></textarea>
           {formState.errors.content && (
             <div>
-              {formState.errors.content?.join(", ")} // Display form errors
-              related to the content field
+              {formState.errors.content?.join(", ")} {/** Display form errors
+              related to the content field */}
             </div>
           )}
         </section>
+
         <section>
+          <label htmlFor="Deadline"> 
+            Date
+            <input
+              type="date"
+              id="deadline"
+              name="deadline"
+              className="text-black bg-white"
+              defaultValue={ initialData.deadline || getCurrentDate() }
+            /> 
+            
+          </label>
+        </section>
+
+        <section> {/** .log statement being used as a bookmark for later stages of DB posting implementation */}
           <button type="submit">Save</button>
           <Link href="/user/todo">Cancel</Link>
         </section>
