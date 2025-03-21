272b26454a0b8e05f584b2fdc2323c74449e1bc6
Harvey Lopez <up2118183@myport.ac.uk>
2025-03-21 01:01:16 +0000

    Added: updated the initialData for TodoForm call, added getDate for deadline functionality and dateTime use later in development

--- src/app/(protected)/list/[listId]/todo/add/page.jsx
+++ src/app/(protected)/list/[listId]/todo/add/page.jsx
@@ -1,15 +1,31 @@
 import { TodoForm } from "@/components";
 import { createTodo } from "@/app/actions/todo";
 
+const getDate = () => {
+  const date = new Date();
+
+  // Values that could be used later in deadline setting  (mins rounded to the nearest ten)
+  const hour = date.getHours();
+  const minute = Math.round(date.getMinutes() / 10) * 10;
+
+  return (date);
+}
+
 export default async function UserTodoAdd({ params }) {
   const { listId } = await params;
 
   const createAction = createTodo.bind(null, listId);
 
+  
   return (
     <TodoForm
       formAction={createAction}
-      initialData={{ title: "", content: "" }}
+      initialData={{ 
+        title: "", 
+        content: "",
+        priority: "Set Priority Here",
+        deadline: { getDate }
+      }}
       listId={listId}
     />
   );
