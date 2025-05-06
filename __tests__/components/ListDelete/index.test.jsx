530bbafe61e2601697e5fb0400f4ffa34f6e7e33
Stephanie <stephaniechloe21@gmail.com>
2025-05-06 15:03:37 +0100

    Fixing the mock

--- __tests__/components/ListDelete/index.test.jsx
+++ __tests__/components/ListDelete/index.test.jsx
@@ -2,6 +2,10 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { ListDelete } from "@/components/ListDelete";
 
+jest.mock("@/app/actions/list", () => ({
+  deleteList: jest.fn(),
+}));
+
 describe("ListDelete", () => {
   it("Renders the ListDelete component", () => {
     render(<ListDelete />);
