f4fd4cbbadb203f9d96091db26d94f20df09b769
Gxry-jun <59424216+Gxry-jun@users.noreply.github.com>
2025-05-09 02:42:09 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,3 @@
-<<<<<<< Updated upstream
-import Logo from "@/components/Logo";
-import { Button } from "@/components/ui/button";
-import { getServerSessionWrapper } from "@/utils";
-import Link from "next/link";
-
-async function LandingPage() {
-  const session = await getServerSessionWrapper();
-
-  return (
-    <div className="h-screen w-screen p-7 space-y-8">
-      <div className="flex items-center justify-between gap-4">
-        <Logo />
-        <Button>
-          {session ? (
-            <Link href={"/dashboard"}>Go to Task Board</Link>
-          ) : (
-            <Link href={"/login"}>Login / Sign up</Link>
-          )}
-        </Button>
-      </div>
-      <div>
-        <h1>This is the home page</h1>
-      </div>
-    </div>
-  );
-}
-
-export default LandingPage;
-=======
 import Logo from "@/components/Logo";
 import { Button } from "@/components/ui/button";
 import { getServerSessionWrapper } from "@/utils";
@@ -152,4 +122,4 @@ async function LandingPage() {
 }
 
 export default LandingPage;
->>>>>>> Stashed changes
+
